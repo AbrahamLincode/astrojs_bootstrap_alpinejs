ccf602501787acdb7f84758d78965b7873831833
AbrahamLincode <abrahamlincode@gmail.com>
2023-04-18 01:06:07 +0300

    Değişikliklerin açıklaması

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  renderers: [
+    '@astrojs/renderer-react',
+    '@astrojs/renderer-svelte',
+    '@astrojs/renderer-vue'
+  ],
+    scripts: [
+        {
+            type : 'module',
+            src: '/node_modules/alpinejs/dist/alpine.js',
+        },
+        {
+            type: 'module',
+            src: '/node_modules/bootstrap/dist/js/bootstrap.js',
+          },
+    ],
+    styles: [
+        {
+          href: '/node_modules/bootstrap/dist/css/bootstrap.min.css',
+          rel: 'stylesheet',
+        },
+      ],
+});
